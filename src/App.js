c7d93ea90f4153c680a8a1914ab89467f6377f79
Hassan Javaid Rao <hassanjrao@gmail.com>
2022-08-05 23:07:51 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balanace";
+import IncomeExpenses from "./components/IncomeExpenses";
+import Transactions from "./components/Transactions";
+import AddTransaction from "./components/AddTransaction";
+import { TransactionProvider } from "./context/TransactionContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TransactionProvider>
+        <Header />
+        <Balance />
+        <IncomeExpenses />
+        <Transactions/>
+        <AddTransaction />
+      </TransactionProvider>
     </div>
   );
 }
